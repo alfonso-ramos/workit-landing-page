90f11502131ae1a223a93a69192825ba0ae668c5
Alfonso Ramos <alfonsoramos.1803@gmail.com>
2024-02-14 18:15:39 -0700

    [ADD] Acomodada de manera primitiva la landing page en vista mobile first

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,19 +3,19 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
-	},
-	colors: {
-		'white': '#fff',
-		'purple': '#24053E',
-		'steel':'#584D62',
-		'primary': '#44FFA1',
-		'pink': '#FCF8FF'
-	},
-	fontSize: {
-		'heading-s': '2rem',
-		'heading-m': '3.5rem',
-		'heading-l': '5rem',
-		base: '1.125rem'
+		colors: {
+			'white': '#fff',
+			'purple': '#24053E',
+			'steel':'#584D62',
+			'primary': '#44FFA1',
+			'pink': '#FCF8FF'
+		},
+		fontSize: {
+			'heading-s': '2rem',
+			'heading-m': '3.5rem',
+			'heading-l': '5rem',
+			base: '1.125rem'
+		},
 	},
 	plugins: [],
 }
